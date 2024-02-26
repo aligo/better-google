29b41252578c75b1a9b860595ac89ce1f23fb4cd
Perry Harrington <35117150+drwonky@users.noreply.github.com>
2024-02-26 10:23:22 -0800

    Removed fork URL and naming
    My fork naming and URL got accidentally merged in a previous commit

--- userscript.js
+++ userscript.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name         Better Google Drwonky
+// @name         Better Google
 // @namespace    google
-// @version      0.1.16.8
+// @version      0.1.16.9
 // @description  Restore google search results to older style with green link below title instead of link above title.  Just tweaks the CSS and does some dynamic JS reordering of the DIVs.
 // @author       aligo, adambh, tejaslodaya, drwonky, yut23
 // @license      MIT
-// @homepageURL   https://github.com/drwonky/better-google
+// @homepageURL   https://github.com/aligo/better-google
 // @match        https://*.google.com/search?*
 // @include      /^https?://(?:www|encrypted|ipv[46])\.google\.[^/]+/(?:$|[#?]|search|webhp)/
 // @grant        none
